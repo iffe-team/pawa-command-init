eaeeb79989ee4cc8575e598fe3073e33330b0098
webplu <wanglin2020@gmail.com>
2016-08-06 23:01:38 +0800

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+# pawa-command-init
+
+PAWA 命令行init插件
+
+## 安装
+放在pawa模块下的node_modules目录中，例如：nodejs\node_modules\pawa\node_modules\pawa-command-init
+
+## Usage
+    
+pawa init -h
+
+    Usage: init <name> [options]
+
+    Options:
+    
+      -h, --help         output usage information
+      -s, --spa          spa app
+
+创建一个SPA应用工程  
+
+```shell
+pawa init projectName --spa   
+```
--- init.js
+++ init.js
@@ -1,15 +1,9 @@
-/*
- * fis
- * http://fis.baidu.com/
- */
-
 'use strict';
 var init = require('./main')
 exports.name = 'init';
 exports.usage = '<names> [path] [options]';
 exports.desc = 'init project';
 exports.register = function(commander){
-    
     commander
         .option('-s, --spa', 'spa for project, used in `init` command')
         .action(function(){
@@ -17,18 +11,13 @@ exports.register = function(commander){
             var options = args.pop();
             var names = args.shift();
             var path = args.shift();
-            var remote = options.spa
-            // || fis.config.get(
-            //     'system.repos',
-            //     fis.project.DEFAULT_REMOTE_REPOS
-            // ).replace(/^\/$/, '') + '/component';
+            // var spa = options.spa
             var dest = process.cwd();
             if(path){
                 if(!fis.util.isAbsolute(path)){
                     dest = fis.util(dest, path);
                 }
             }
-            // console.log(options)
             init.createProject(names, commander)
            
         });
